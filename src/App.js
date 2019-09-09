860905ac291ae12a5008d707c555a0e096ace2a3
AllanRaf <rafale.uk@googlemail.com>
2019-09-09 13:58:37 +0200

    first react app created

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <p className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!
         </a>
-      </header>
+      </p>
     </div>
   );
 }
